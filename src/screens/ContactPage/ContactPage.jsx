2fdb46838f2788ea3190504a75846277c913c9bf
rmhermes <rmhermes94@gmail.com>
2023-07-19 23:15:13 -0300

    contactpage

--- src/screens/ContactPage/ContactPage.jsx
+++ src/screens/ContactPage/ContactPage.jsx
@@ -2,7 +2,19 @@ import React from 'react'
 import './contactPage.css'
 const ContactPage = () => {
   return (
-    <div className='contactPage'>ContactPage</div>
+    <div className='contactPage'>
+      <form>
+        <label htmlFor="name">Nombre:</label>
+        <input type="text" />
+        <label htmlFor="lastname">Apellido: </label>
+        <input type="text" />
+        <label htmlFor="telefono">Teléfono: </label>
+        <input type="number" />
+        <label htmlFor="mail">Correo Electrónico:</label>
+        <input type="email" required  />
+        <textarea name="" cols="50" rows="10">Ingrese su comentario</textarea>
+        </form>
+    </div>
   )
 }
 
