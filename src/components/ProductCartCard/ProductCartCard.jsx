b3acba45f74c0ecf4586b5aaa37a805c16d40e9f
rmhermes <rmhermes94@gmail.com>
2023-07-21 18:56:07 -0300

    update

--- src/components/ProductCartCard/ProductCartCard.jsx
+++ src/components/ProductCartCard/ProductCartCard.jsx
@@ -5,7 +5,7 @@ const ProductCartCard = ({producto}) => {
     return(
         <div className="cartCard">
             <h2>{producto.nombre}</h2>
-            <img src={producto.img} alt="imgplant" />
+            <img className='imgCardCart' src={producto.img} alt="imgplant" />
             <h3>Precio: ${producto.precio}</h3>
             <p>Cantidad: {producto.quantity}</p>
             
