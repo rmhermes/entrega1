ddf12a6c0be81bf542c3525293d818adde10d303
rmhermes <rmhermes94@gmail.com>
2023-07-23 14:13:01 -0300

    update x1000

--- src/screens/DetailPage/DetailPage.jsx
+++ src/screens/DetailPage/DetailPage.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react'
+    import React, { useState } from 'react'
 import { useParams } from 'react-router-dom'
 import { useCustomContext } from '../../ContextManager/ContextProvider'
 import { Counter } from '../../components'
@@ -11,19 +11,22 @@ const DetailPage =()=>{
     return(
         <div className='detailPage'>
             <div className='contDetail'>
-            <img className='imgCard' src={productDetail.img} alt={producto.nombre} />
+            
+        <div className='counterImg'>
+        <img className='imgCard' src={productDetail.img} alt={productDetail.nombre} />
+            { isInCart(id)
+        ?        
+            <Counter initialValue = {productDetail.quantity} stock= {productDetail.stock} id= {productDetail.id}/> 
+            :
+            <Counter initialValue={1} stock ={productDetail.stock} id={productDetail.id}/>}
+            
+            </div>
             <h2>{productDetail.nombre}</h2>
             <h3>Precio: ${productDetail.precio}</h3>
             <p>Descripción: {productDetail.descripcion}</p>
             <span className='spanHistoria'>Historia: {productDetail.historia}</span>
 
-        { isInCart(id)
-        ? 
-        
-            <Counter initialValue = {productDetail.quantity} stock= {productDetail.stock} id= {productDetail.id}/> 
-            :
-            <Counter initialValue={1} stock ={productDetail.stock} id={productDetail.id}/>
-    }
+    
         </div>
         </div>
     )
